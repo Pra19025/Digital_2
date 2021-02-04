1aae7df0802bcb3d533cc5f64f27946a3bfaa71b
Noel Prado <68123083+Pra19025@users.noreply.github.com>
2021-02-04 16:50:50 -0600

    Lab3 inicio

---
+++ Lab3LCD.X/main.c
@@ -0,0 +1,92 @@
+/*
+ * File:   main.c
+ * Author: Noel Prado
+ *
+ * Created on 4 de febrero de 2021, 09:29 AM
+ */
+
+
+#include <xc.h>
+#include <stdint.h>
+#include "LCD8bits.h"
+
+// CONFIG1
+#pragma config FOSC = INTRC_NOCLKOUT// Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown Out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+
+// CONFIG2
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+
+#define _XTAL_FREQ 4000000
+
+//**********************************************************************************************************************************************
+// Variables 
+//**********************************************************************************************************************************************
+
+
+
+//**********************************************************************************************************************************************
+// Prototipos de funciones 
+//**********************************************************************************************************************************************
+
+void Setup(void);
+
+//**************************************************************
+// Vector de interrupción
+//**************************************************************
+
+void __interrupt() ISR(void) {
+
+}
+
+void main(void) {
+    Setup();
+    __delay_ms(100);
+    Lcd_Init();
+
+    while (1) {
+
+        Lcd_Clear();
+        __delay_ms(100);
+        Lcd_Set_Cursor(1, 1);
+        Lcd_Write_String("LCD Library for");
+        __delay_ms(100);
+        Lcd_Set_Cursor(2, 1);
+        Lcd_Write_String("MPLAB XC8");
+        __delay_ms(2000);
+        Lcd_Clear();
+        Lcd_Set_Cursor(1, 1);
+        Lcd_Write_String("Developed By");
+        Lcd_Set_Cursor(2, 1);
+        Lcd_Write_String("electroSome");
+        __delay_ms(2000);
+        Lcd_Clear();
+        Lcd_Set_Cursor(1, 1);
+        Lcd_Write_String("www.electroSome.com");
+
+
+    }
+
+    return;
+}
+
+void Setup(void) {
+
+    TRISA = 255;
+    TRISD = 0;
+    TRISC = 0;
+
+    PORTA = 0;
+    PORTC = 0;
+    PORTD = 0;
+
+}
