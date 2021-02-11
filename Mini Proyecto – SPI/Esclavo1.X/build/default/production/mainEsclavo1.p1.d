164f7a469044edf47e7ecc95117b04fb4cc20289
Noel Prado <68123083+Pra19025@users.noreply.github.com>
2021-02-11 09:00:14 -0600

    inicio del proyecto

---
+++ Mini Proyecto – SPI/Esclavo1.X/build/default/production/mainEsclavo1.p1.d
@@ -0,0 +1,2 @@
+build/default/production/mainEsclavo1.p1:  \
+mainEsclavo1.c 
---
+++ Mini Proyecto – SPI/Esclavo1.X/mainEsclavo1.c
@@ -0,0 +1,13 @@
+/*
+ * File:   mainEsclavo1.c
+ * Author: Noel Prado
+ *
+ * Created on 11 de febrero de 2021, 08:51 AM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
