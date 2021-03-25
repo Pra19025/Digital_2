8a4d5a11cc2607dad101d18315378d536cf5c0ab
Noel Prado <68123083+Pra19025@users.noreply.github.com>
2021-03-25 17:57:01 -0600

    codigo terminado

--- Lab5SD/Lab5SD.ino
+++ Lab5SD/Lab5SD.ino
@@ -1,29 +1,11 @@
-/*
-  Listfiles
+// Noel Prado
+//19025
+//Laboratorio 5
 
-  This example shows how print out the files in a
-  directory on a SD card
 
-  The circuit:
-   SD card attached to SPI bus as follows:
- ** MOSI - pin 11
- ** MISO - pin 12
- ** CLK - pin 13
- ** CS - pin 4
-
-  created   Nov 2010
-  by David A. Mellis
-  modified 9 Apr 2012
-  by Tom Igoe
-  modified 2 Feb 2014
-  by Scott Fitzgerald
-
-  This example code is in the public domain.
-
-*/
 #include <SPI.h>
 #include <SD.h>
-
+File root;
 File myFile;
 int inByte = 0;
 int control = 0;
@@ -33,129 +15,124 @@ void setup()
   Serial.begin(115200);
   SPI.setModule(0);
 
-
-  Serial.print("Initializing SD card...");
-  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
-  // Note that even if it's not used as the CS pin, the hardware SS pin
-  // (10 on most Arduino boards, 53 on the Mega) must be left as an output
-  // or the SD library functions will not work.
   pinMode(PA_3, OUTPUT);
 
   if (!SD.begin(PA_3)) {
     Serial.println("initialization failed!");
     return;
   }
-  Serial.println("initialization done.");
-
+  //root = SD.open("/");
+  printDirectory();
+    Serial.println();
+    printDirectory();
+    
   Serial.println();
-  Serial.println();
-  Serial.println();
 
-
-  myFile = SD.open("/");
-
-
-  //Serial.println("done!");
 }
 
 void loop()
 {
+  if (control == 0) {
+    Serial.println();
+    Serial.println("**************************************");
+    Serial.println("Seleccione que dibujo desea visualziar");
+    Serial.println("**************************************");
+    Serial.println();
+    Serial.println();
+    printDirectory();
+    Serial.println();
+    Serial.println("**************************************");
+    control = 1;
 
+  }
   //se revisa que selecciona el usuario
   if (Serial.available()) {
     inByte = Serial.read();
-    Serial.write(inByte);
-    
-
-    if (inByte == '1') {
-
-      
-      myFile = SD.open("1mew.txt");
-      if (myFile) {
-        Serial.println("1mew.txt:");
-
-        // read from the file until there's nothing else in it:
-        while (myFile.available()) {
-          Serial.write(myFile.read());
-        }
-        // close the file:
-        myFile.close();
-      } else {
-        // if the file didn't open, print an error:
-        Serial.println("error opening test.txt");
-      }
-    }
-
-      if (inByte == '2') {
-
-      
-      myFile = SD.open("2icono.txt");
-      if (myFile) {
-        Serial.println("2trifuerza.txt:");
-
-        // read from the file until there's nothing else in it:
-        while (myFile.available()) {
-          Serial.write(myFile.read());
-        }
-        // close the file:
-        myFile.close();
-      } else {
-        // if the file didn't open, print an error:
-        Serial.println("error opening test.txt");
-      }
-    }
-
-        if (inByte == '3') {
-
-      
-      myFile = SD.open("3pikachu.txt");
-      if (myFile) {
-        Serial.println("3pikachu.txt:");
-
-        // read from the file until there's nothing else in it:
-        while (myFile.available()) {
-          Serial.write(myFile.read());
-        }
-        // close the file:
-        myFile.close();
-      } else {
-        // if the file didn't open, print an error:
-        Serial.println("error opening test.txt");
-      }
-    }
-    
-
+  
   }
-  if (control == 0) {
-    Serial.println("Seleccione que dibujo desea visualziar");
-    printDirectory(myFile, 0);
-    control = 1;
-    delay(1000);
-  }
+ if (inByte == '1') {
+      control = 0;
+      
+      myFile = SD.open("/mew.txt");
+      if (myFile) {
+        // read from the file until there's nothing else in it:
+        while (myFile.available()) {
+          Serial.write(myFile.read());
+        }
+        // close the file:
+        myFile.close();
+      } else {
+        // if the file didn't open, print an error:
+        Serial.println("error opening test.txt");
+      }
+      Serial.println();
+      inByte = 0;
+    }
+
+    if (inByte == '2') {
+      control = 0;
+
+      myFile = SD.open("/icon.txt");
+      if (myFile) {
+        // read from the file until there's nothing else in it:
+        while (myFile.available()) {
+          Serial.write(myFile.read());
+        }
+        // close the file:
+        myFile.close();
+      } else {
+        // if the file didn't open, print an error:
+        Serial.println("error opening test.txt");
+      }
+      Serial.println();
+      inByte = 0;
+    }
+
+    if (inByte == '3') {
+      control = 0;
+
+      myFile = SD.open("/pikachu.txt");
+      if (myFile) {
+        // read from the file until there's nothing else in it:
+        while (myFile.available()) {
+          Serial.write(myFile.read());
+        }
+        // close the file:
+        myFile.close();
+      } else {
+        // if the file didn't open, print an error:
+        Serial.println("error opening test.txt");
+      }
+      Serial.println();
+      inByte = 0;
+    }
 
 
 }
 
-void printDirectory(File dir, int numTabs) {
+void printDirectory() {
+  delay(250);
+  myFile = SD.open("/");
+  myFile.rewindDirectory(); 
+    
   while (true) {
 
-    File entry =  dir.openNextFile();
+    File entry =  myFile.openNextFile();
     if (! entry) {
       // no more files
-      break;
+      return;//break;
     }
-    for (uint8_t i = 0; i < numTabs; i++) {
+    /*for (uint8_t i = 0; i < numTabs; i++) {
       Serial.print('\t');
-    }
-    Serial.print(entry.name());
-    if (entry.isDirectory()) {
+    }*/
+    Serial.println(entry.name());
+    /*if (entry.isDirectory()) {
       Serial.println("/");
       printDirectory(entry, numTabs + 1);
     } else {
       // files have sizes, directories do not
       Serial.print("\n");
-      //      Serial.println(entry.size(), DEC);
-    }
+    }*/
     entry.close();
   }
 }
